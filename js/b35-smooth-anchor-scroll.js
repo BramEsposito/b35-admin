fcf9187175e4b7fe909af80ca1efe2066adcd38d
Bram Esposito <bram@bramesposito.com>
2021-04-01 20:06:44 +0200

    improve scrolling to anchors, allow to set offset

--- js/b35-smooth-anchor-scroll.js
+++ js/b35-smooth-anchor-scroll.js
@@ -1,12 +1,26 @@
+/**
+ * Scrolls anchors into view
+ * 
+ * Set window.scrollOffset to the amount of pixels you want to offset 
+ * between the window top and the target element
+ */
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    console.log("adding event listeners");
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+      const element = document.querySelector(this.getAttribute('href'));
+
+      if (window.scrollOffset != undefined) {
+
+        const y = element.getBoundingClientRect().top + window.pageYOffset + window.scrollOffset;
+
+        window.scrollTo({top: y, behavior: 'smooth'});
+      } else {
+        element.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
     });
   });
 });
