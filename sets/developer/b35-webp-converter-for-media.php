404e92183e2e19eb362650252599cb9490bd70c7
Bram Esposito <bram@bramesposito.com>
2021-11-23 15:17:44 +0100

    fix for WebP conversion

--- sets/developer/b35-webp-converter-for-media.php
+++ sets/developer/b35-webp-converter-for-media.php
@@ -1,9 +1,24 @@
 <?php
 
 /*
- * Alter path to wite root for webp-converter-for-media plugin
+ * Alter path to site root for webp-converter-for-media plugin
  */
 add_filter( 'webpc_site_root', function( $path ) {
   // get_home_path() is not available in REST/CLI mode
   return dirname( ABSPATH );
+});
+
+/*
+ * Alter url to upload directory for webp-converter-for-media plugin
+ */
+add_filter( 'webpc_dir_url',  function($path) {
+  $uploaddir = wp_upload_dir();
+  return $uploaddir['baseurl'];
+});
+
+/*
+ * Alter path to upload directory for webp-converter-for-media plugin
+ */
+add_filter( 'webpc_dir_path',  function($path) {
+  return realpath(WP_CONTENT_DIR."/uploads-webpc");
 });
