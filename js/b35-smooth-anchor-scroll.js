70657ab01d556de315dc23fda355c2a9ebe468a1
Bram Esposito <bram@bramesposito.com>
2022-10-09 12:04:05 +0200

    fix handling of click event

--- js/b35-smooth-anchor-scroll.js
+++ js/b35-smooth-anchor-scroll.js
@@ -26,20 +26,23 @@ document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
+      e.stopPropagation();
 
       if( this.getAttribute('href') === "#" ) return;
 
       const element = document.querySelector(this.getAttribute('href'));
 
-      if (b35_smooth_anchor_scroll.scrollOffset != undefined) {
+      if (b35_smooth_anchor_scroll != undefined && b35_smooth_anchor_scroll.scrollOffset != undefined) {
 
         const y = element.getBoundingClientRect().top + window.pageYOffset + parseInt(b35_smooth_anchor_scroll.scrollOffset);
 
         window.scrollTo({top: y, behavior: 'smooth'});
+        return false;
       } else {
         element.scrollIntoView({
           behavior: 'smooth'
         });
+        return false;
       }
     });
   });
@@ -53,8 +56,6 @@ document.addEventListener("DOMContentLoaded", function() {
         if (element) {
           const y = element.getBoundingClientRect().top + window.pageYOffset + parseInt(b35_smooth_anchor_scroll.scrollOffset);
 
-          console.log("destination:", element.getBoundingClientRect().top);
-          console.log("scrolling to", y);
           window.scrollTo({top: y, behavior: 'smooth'});
         }
       });
