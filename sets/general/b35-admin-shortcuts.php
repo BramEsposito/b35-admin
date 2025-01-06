c79f36e4c898c00d18fc4b3774dca49ae144ca1b
Bram Esposito <bram@bramesposito.com>
2025-01-06 19:36:47 +0100

    fix overlay on edit loading screen

--- sets/general/b35-admin-shortcuts.php
+++ sets/general/b35-admin-shortcuts.php
@@ -19,7 +19,7 @@ function b35_admin_shortcuts_enqueue_js() {
 function b35_admin_shortcuts_enqueue_modal() {
   if (is_singular('post')) {
     ?>
-    <div id="b35-admin-shortcuts-modal" aria-hidden="true" tabindex="-1" style="display:none;position:absolute;background-color:rgba(202, 202, 202, 0.59);place-items:center;top:0;bottom:0;left:0;right:0;">
+    <div id="b35-admin-shortcuts-modal" aria-hidden="true" tabindex="-1" style="display:none;position:fixed;background-color:rgba(202, 202, 202, 0.59);place-items:center;top:0;bottom:0;left:0;right:0;">
       <span style="background-color:rgb(247, 247, 247);padding: 15px;border-radius:5px;">LOADING...</span>
     </div>
     <?php
